6656efb8640dfca34e5ed5828aa39e256b321b60
gabrfernandez <61893056+gabrfernandez@users.noreply.github.com>
2020-11-11 14:47:13 -0800

    templates

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context={
+        'message':'This is my first message',
+        'message2':'Second message',
+    }
+    return render(request, 'posts/index.html', context)
