00c83a4040e46646fe346c277bb7b123adb5fd5b
gabrfernandez <61893056+gabrfernandez@users.noreply.github.com>
2020-11-11 15:29:07 -0800

    customize admin post page

--- posts/admin.py
+++ posts/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+#customize admin page to display search and filter by certain fields
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date', 'id']
+    list_display_links = ['title', 'date', 'id']
+    search_fields = ['title', 'id']
+    list_filter = ['date']
+admin.site.register(Post, PostAdmin)
+
