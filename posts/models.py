e7a12217bc0c444d03df3c4b71c794c942636443
gabrfernandez <61893056+gabrfernandez@users.noreply.github.com>
2020-11-11 17:38:56 -0800

    upload images

--- posts/models.py
+++ posts/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
     date = models.DateField(auto_now_add=True)
+    image = models.ImageField(null=True, blank=True, upload_to='images')
 
     def __str__(self):
         return self.title
