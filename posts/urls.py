d19d2f9d20eb61c63ab571290b11e98cc5115fed
gabrfernandez <61893056+gabrfernandez@users.noreply.github.com>
2020-11-11 18:50:23 -0800

    detail view

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from posts import views
 urlpatterns=[
-    path('', views.index)
+    path('', views.index, name="index"),
+    path('detail/<int:id>', views.detail_view, name="detail"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+
 
 # Create your views here.
 def index(request):
-
     context={
         'posts':Post.objects.all()
     }
     return render(request, 'posts/index.html', context)
+
+def detail_view(request, id):
+    #post = Post.objects.get(id=id)
+    post= get_object_or_404(Post, id=id)
+    context={
+        'post':post
+    }
+    return render(request, 'posts/detail.html', context)
